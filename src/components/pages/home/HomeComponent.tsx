2fdcb18f908e0b9b34d9b86290382815b5d81174
Jibon <mailme.jibon@gmail.com>
2025-07-02 14:52:41 +0600

    component updated

--- src/components/pages/home/HomeComponent.tsx
+++ src/components/pages/home/HomeComponent.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import Banner from "./Banner/Banner";
-import BenefitsComponent from "./Benefits/BenefitsComponent";
-import HowItsWorks from "./HowItsWorks/HowItsWorks";
-import Pricing from "./Pricing/Pricing";
 
 // import { Container } from "@/components/ui-library/container";
 
@@ -10,9 +7,6 @@ const HomeComponent = () => {
   return (
     <div className="">
       <Banner />
-      <BenefitsComponent/>
-      <HowItsWorks/>
-      <Pricing/>
     </div>
   );
 };
