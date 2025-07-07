154d876574dd806a60dbf8e544dc441be6640a63
Jibon <mailme.jibon@gmail.com>
2025-07-07 15:09:06 +0600

    loading updated

--- src/components/shared/loading/Loading.tsx
+++ src/components/shared/loading/Loading.tsx
@@ -1,10 +1,9 @@
-import { Spin } from "antd";
 import React from "react";
 
 const Loading = () => {
   return (
     <div className="h-[calc(100vh-170px)] w-full flex items-center justify-center z-50">
-      <Spin size="large" />
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
     </div>
   );
 };
